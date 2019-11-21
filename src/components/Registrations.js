52faae5c41ae87b6d1c03ff1544e610741bd01fb
Rachel Selkirk <rachel.selkirk@gmail.com>
2019-11-21 12:54:09 -0800

    fetching api data

--- src/components/Registrations.js
+++ src/components/Registrations.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class Registrations extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://demo14.secure.retreat.guru/api/v1/registrations?token=ef061e1a717568ee5ca5c76a94cf5842')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+  
   render() {
     return (
       <div>
-        Registrations
+        {JSON.stringify(this.state.data)}
       </div>
     )
   }
